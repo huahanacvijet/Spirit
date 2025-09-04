3e41b246b149ad76013a6b41da02332642449185
Hana Rajic <haisrajic@gmail.com>
2025-09-05 00:41:00 +0100

    Removes comments in preload.js

--- preload.js
+++ preload.js
@@ -1,6 +1,6 @@
 const {contextBridge, ipcRenderer } = require("electron");
 
-// API for front end (bridge)
+// Connect front to back
 contextBridge.exposeInMainWorld("electronAPI", {
     // this ipcR... calls handler in electorn.js
     askLLM: (conversation) => ipcRenderer.invoke("ask-llm", conversation),
